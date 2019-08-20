547b59df0379fc9b8229c9d80dee3dd15437a4cf
diditx87 <42115824+diditx87@users.noreply.github.com>
2019-08-20 15:21:14 +0700

    Update gofood.php

--- gofood.php
+++ gofood.php
@@ -42,7 +42,6 @@ $tools = trim(fgets(STDIN));
 				// Claim Voucher
 				$token = $verifs->data->access_token;
 				$headers[] = 'Authorization: Bearer '.$token;
-echo $token;
 				$data3 = '{"promo_code":"GOFOODNASGOR07"}';
 				$claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', $data3, $headers);
 				$claims = json_decode($claim[0]);
@@ -83,7 +82,6 @@ echo $token;
 				// Claim Voucher
 				$token = $verifs->data->access_token;
 				$headers[] = 'Authorization: Bearer '.$token;
-echo $token;
 				$data3 = '{"promo_code":"GOFOODNASGOR07"}';
 				$claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', $data3, $headers);
 				$claims = json_decode($claim[0]);
